9c2005f620f0015b72ed17c55bb8e8493f75c0ae
lichaa824 <lisandrorubi@gmail.com>
2022-11-08 16:09:55 -0300

    Add categories test

---
+++ test/categories.test.js
@@ -0,0 +1,93 @@
+const { Category } = require("../database/models");
+const chai = require("chai");
+const chaiHttp = require("chai-http");
+const server = require("../app");
+const should = chai.should();
+
+chai.use(chaiHttp);
+
+let testCategory;
+
+describe("Categories endpoint Endpoint", () => {
+  describe("POST /categories", () => {
+    it("should make a new Category", (done) => {
+      chai
+        .request(server)
+        .post("/categories")
+        .send({
+          name: "test",
+          description: "test category",
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        })
+        .end((err, res) => {
+          testCategory = res.body.body;
+          res.should.have.status(200);
+          res.body.body.should.be.a("object");
+          done();
+        });
+    });
+  });
+
+  describe("GET /category", () => {
+    it("should GET all categories", (done) => {
+      chai
+        .request(server)
+        .get("/categories")
+        .end((err, res) => {
+          res.should.have.status(200);
+          res.body.body.should.be.a("array");
+          done();
+        });
+    });
+    it("should GET categories by given ID", (done) => {
+      chai
+        .request(server)
+        .get("/categories/" + testCategory.id)
+        .end((err, res) => {
+          res.should.have.status(200);
+          res.body.body.description.should.be.eql("test category");
+          done();
+        });
+    });
+    it("should return an error if the ID is invalid", (done) => {
+      chai
+        .request(server)
+        .get("/categories/" + 9999)
+        .end((err, res) => {
+          res.should.have.status(404);
+          done();
+        });
+    });
+  });
+
+  describe("PUT /categories", () => {
+    it("should update category by given ID", (done) => {
+      chai
+        .request(server)
+        .put("/categories/" + testCategory.id)
+        .send({ ...testCategory, name: "test category updated" })
+        .end((err, res) => {
+          res.should.have.status(200);
+          done();
+        });
+    });
+  });
+
+  describe("DELETE /categories", () => {
+    it("should delete categories by given ID", (done) => {
+      chai
+        .request(server)
+        .delete("/categories/" + testCategory.id)
+        .end((err, res) => {
+          res.should.have.status(200);
+          done();
+        });
+    });
+  });
+
+  after((done) => {
+    Category.destroy({ where: { id: testCategory.id } });
+    done();
+  });
+});
